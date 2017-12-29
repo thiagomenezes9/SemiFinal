475d410dda1ea7a033454fd7fcd657cd16d575be
Thiago Menezes <thiagomenezes9@gmail.com>
2017-12-29 08:31:43 -0200

    começar post

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -13,7 +13,8 @@
                 <div class="pull-left info">
                     <p>{{ Auth::user()->name }}</p>
                     <!-- Status -->
-                    <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}</a>
+                    <span>{{Auth::user()->type}}</span>
+                    {{--<a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}</a>--}}
                 </div>
             </div>
         @endif
@@ -37,11 +38,34 @@
                 {{--MENU AKI DEPOIS--}}
 
 
-                    <a href="{{route('posts.index')}}">
-                        <i class="fa fa-newspaper-o"></i>
-                        <span>Posts</span>
+                    {{--<a href="{{route('posts.index')}}">--}}
+                        {{--<i class="fa fa-newspaper-o"></i>--}}
+                        {{--<span>Posts</span>--}}
                         {{--<i class="fa fa-angle-left pull-right"></i>--}}
-                    </a>
+                    {{--</a>--}}
+
+
+                <a href="#">
+                    <i class="fa fa-newspaper-o"></i>
+                    <span>Posts</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+
+                <ul class="treeview-menu">
+                    <li class="{{ $menuConItem1 or '' }}">
+                        <a href="#">
+                            <i class='fa fa-newspaper-o'></i>
+                            <span>Meus Posts</span>
+                        </a>
+                    </li>
+                    <li class="{{ $menuConItem2 or '' }}">
+                        <a href="#">
+                            <i class='fa fa-book'></i>
+                            <span>Todos Posts</span>
+                        </a>
+                    </li>
+                </ul>
+
 
 
                      @if(Auth::user()->type == 'ADMIN')
